629787a5afaa3390ea3aa143223e76ce855a9069
KangJeemin <wlals1234560@gmail.com>
2023-05-23 16:21:10 +0900

    template로 render하기

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+from pymongo import MongoClient
+
+
+app= Flask(__name__)
+client=MongoClient("mongodb://localhost:27017/")
+db=client['test-db']
+
+db1= db.post.find_one({'author':'jimin'})['tags']
+
+
+
+
+
+@app.route('/')
+def home():
+  return render_template('html.html',name=db1) 
+
+
+
+
+if __name__=='__main__':
+  app.run(debug=True)
+
