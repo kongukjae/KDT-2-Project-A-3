db0beddaa1bb113b625fde4fd957efd7dd2c19c8
KangJeemin <wlals1234560@gmail.com>
2023-05-23 16:55:12 +0900

    삼성전자 현재가 주식 가져옴, pandas,mojito2,설치

---
+++ api.py
@@ -0,0 +1,12 @@
+import mojito 
+import pprint
+
+
+key ="PSVT5oQXN4N39r3jhoLtrCiVen4fcJ3p7zOh"
+secret = "OeeQY05O9OEfjuOP2KEtVpbP77p8WKaClPqgOEdSAVdH/FazfG51bqSc97t16uYOsvjb5DzrbqB11cfuMfBXPtwDB2BQqg7otSZAHo61OkobqBGPWJHGOHE/lt+X4WPNhyDiDu06EMiC6t+lvcIrG50t4/alJf7qhfL/dkg8sfOJgC66SDA="
+acc_no ="12345678-01"
+
+broker = mojito.KoreaInvestment(api_key=key, api_secret=secret, acc_no=acc_no)
+resp = broker.fetch_price("005930")
+pprint.pprint(resp)
+
