f25d074fefd78619386bc1fa921d37417f0191e7
KyoungTaekLee <dlrudxor0302@gmail.com>
2023-05-24 14:14:49 +0900

    데이터 json화 및 테이블 두가지 방식

--- app.py
+++ app.py
@@ -1,25 +1,20 @@
-# import mojito
-# import pprint
-
-# key = "PSVT5oQXN4N39r3jhoLtrCiVen4fcJ3p7zOh"
-# secret = "OeeQY05O9OEfjuOP2KEtVpbP77p8WKaClPqgOEdSAVdH/FazfG51bqSc97t16uYOsvjb5DzrbqB11cfuMfBXPtwDB2BQqg7otSZAHo61OkobqBGPWJHGOHE/lt+X4WPNhyDiDu06EMiC6t+lvcIrG50t4/alJf7qhfL/dkg8sfOJgC66SDA="
-# acc_no = "12345678-01123123"
-
-# broker = mojito.KoreaInvestment(api_key=key, api_secret=secret, acc_no=acc_no)
-# resp = broker.fetch_price("005930")
-# pprint.pprint(resp)
-
 import mojito
+import pprint
+import json
 
+# 값들을 세팅
 key = "PSVT5oQXN4N39r3jhoLtrCiVen4fcJ3p7zOh"
 secret = "OeeQY05O9OEfjuOP2KEtVpbP77p8WKaClPqgOEdSAVdH/FazfG51bqSc97t16uYOsvjb5DzrbqB11cfuMfBXPtwDB2BQqg7otSZAHo61OkobqBGPWJHGOHE/lt+X4WPNhyDiDu06EMiC6t+lvcIrG50t4/alJf7qhfL/dkg8sfOJgC66SDA="
-ACC_NO = "12345678-01"    # 8자리-2자리
+acc_no = "12345678-01123123"
 
-broker = mojito.KoreaInvestment(
-    api_key = key,
-    api_secret = secret,
-    acc_no = ACC_NO
-)
+broker = mojito.KoreaInvestment(api_key=key, api_secret=secret, acc_no=acc_no)
+resp = broker.fetch_price("005930")
 
-symbols = broker.fetch_symbols()
-symbols
+# resp 값을 정렬시키기
+sorted_data = json.dumps(resp, sort_keys=True, indent=2)
+
+# 정렬된 데이터를 json 파일로 생성
+with open("sorted_data.json", "w") as file:
+    file.write(sorted_data)
+    
+pprint.pprint(sorted_data)
---
+++ test.py
@@ -0,0 +1,19 @@
+import mojito
+import json
+
+key = "PSVT5oQXN4N39r3jhoLtrCiVen4fcJ3p7zOh"
+secret = "OeeQY05O9OEfjuOP2KEtVpbP77p8WKaClPqgOEdSAVdH/FazfG51bqSc97t16uYOsvjb5DzrbqB11cfuMfBXPtwDB2BQqg7otSZAHo61OkobqBGPWJHGOHE/lt+X4WPNhyDiDu06EMiC6t+lvcIrG50t4/alJf7qhfL/dkg8sfOJgC66SDA="
+acc_no = "12345678-01123123"   # 8자리-2자리
+
+broker = mojito.KoreaInvestment(
+    api_key = key,
+    api_secret = secret,
+    acc_no = acc_no
+)
+dir(broker)
+symbols = broker.fetch_symbols()
+resp = broker.fetch_price("005930")
+print(symbols, resp)
+print(type(resp))
+print(type(symbols))
+
