a1a7a0f8259e350ad1a7a271d8f21af5c2ab311f
ujou173 <ujou173@gmail.com>
2023-05-24 14:15:20 +0900

    데이터 가공 후 추출 성공

--- stock_test.py
+++ stock_test.py
@@ -9,18 +9,34 @@ broker = mojito.KoreaInvestment(api_key=key, api_secret=secret, acc_no=acc_no)
 # print(dir(broker))
 resp = broker.fetch_price("005930")
 # pprint.pprint(resp)
-print("시가   :  ", resp['output']['stck_oprc'])    # 시가
-print("현재가 :  ", resp['output']['stck_prpr'])    # 시가
-print("최고가 : ", resp['output']['stck_hgpr'])     # 고가
-print("최저가 : ", resp['output']['stck_lwpr'])     # 저가
-print("종가   : ", resp['output']['stck_prpr'])     # 종가
+# print("시가   :  ", resp['output']['stck_oprc'])    # 시가
+# print("현재가 :  ", resp['output']['stck_prpr'])    # 시가
+# print("최고가 : ", resp['output']['stck_hgpr'])     # 고가
+# print("최저가 : ", resp['output']['stck_lwpr'])     # 저가
+# print("종가   : ", resp['output']['stck_prpr'])     # 종가
 
 symbols = broker.fetch_kosdaq_symbols()        # 코스닥
 # print(dir(symbols))
-myArr = []
-# 객체나 배열을 이용해 단축코드와 한글명만 뽑아서 하나의 데이터 객체를 구성할 수 있을 것으로 예상 됨
-# for num in symbols:
-  # num
-print(symbols['단축코드'])
-print(symbols['한글명'])
+
+class myObject:
+    def __init__(self):
+        self.data = {}
+
+    def __str__(self):
+        return str(self.data)
+
+my_object = myObject()
+
+for i in range(len(symbols)):
+    key = symbols['한글명'][i]
+    value = {
+        '단축코드': symbols['단축코드'][i],
+        '시가총액': symbols['시가총액'][i]
+    }
+    my_object.data[key] = value
+
+print(my_object.data['헝셩그룹']['단축코드'])
+# print(my_object[])
+# print(symbols['단축코드'])
+# print(symbols['한글명'])
 # symbols.head()
