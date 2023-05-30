c74ba2a3efe6ad9af4a4ae3ff0afb1d232250311
KangJeemin <wlals1234560@gmail.com>
2023-05-30 15:39:21 +0900

    비밀번호 유효성 검사 완료. /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/ 의 의미 공부 필요

--- ChickenStock/App.tsx
+++ ChickenStock/App.tsx
@@ -1,164 +1,29 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
 
-// import React from 'react';
-// import type {PropsWithChildren} from 'react';
-// import {
-//   SafeAreaView,
-//   ScrollView,
-//   StatusBar,
-//   StyleSheet,
-//   Text,
-//   useColorScheme,
-//   View,
-// } from 'react-native';
-
-// import {
-//   Colors,
-//   DebugInstructions,
-//   Header,
-//   LearnMoreLinks,
-//   ReloadInstructions,
-// } from 'react-native/Libraries/NewAppScreen';
-
-// type SectionProps = PropsWithChildren<{
-//   title: string;
-// }>;
-
-// function Section({children, title}: SectionProps): JSX.Element {
-//   const isDarkMode = useColorScheme() === 'dark';
-//   return (
-//     <View style={styles.sectionContainer}>
-//       <Text
-//         style={[
-//           styles.sectionTitle,
-//           {
-//             color: isDarkMode ? Colors.white : Colors.black,
-//           },
-//         ]}>
-//         {title}
-//       </Text>
-//       <Text
-//         style={[
-//           styles.sectionDescription,
-//           {
-//             color: isDarkMode ? Colors.light : Colors.dark,
-//           },
-//         ]}>
-//         {children}
-//       </Text>
-//     </View>
-//   );
-// }
-
-// function App(): JSX.Element {
-//   const isDarkMode = useColorScheme() === 'dark';
-
-//   const backgroundStyle = {
-//     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-//   };
-
-//   return (
-//     <SafeAreaView style={backgroundStyle}>
-//       <StatusBar
-//         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-//         backgroundColor={backgroundStyle.backgroundColor}
-//       />
-//       <ScrollView
-//         contentInsetAdjustmentBehavior="automatic"
-//         style={backgroundStyle}>
-//         <Header />
-//         <View
-//           style={{
-//             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-//           }}>
-//           <Section title="Step One">
-//             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-//             screen and then come back to see your edits.
-//           </Section>
-//           <Section title="See Your Changes">
-//             <ReloadInstructions />
-//           </Section>
-//           <Section title="Debug">
-//             <DebugInstructions />
-//           </Section>
-//           <Section title="Learn More">
-//             Read the docs to discover what to do next:
-//           </Section>
-//           <LearnMoreLinks />
-//         </View>
-//       </ScrollView>
-//     </SafeAreaView>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   sectionContainer: {
-//     marginTop: 32,
-//     paddingHorizontal: 24,
-//   },
-//   sectionTitle: {
-//     fontSize: 24,
-//     fontWeight: '600',
-//   },
-//   sectionDescription: {
-//     marginTop: 8,
-//     fontSize: 18,
-//     fontWeight: '400',
-//   },
-//   highlight: {
-//     fontWeight: '700',
-//   },
-// });
-
-// export default App;
-
-// import React, { useEffect, useState } from 'react';
-// import { View, Text } from 'react-native';
-
-// const App = () => {
-//   const [data, setData] = useState(null);
-
-//   useEffect(() => {
-//     fetch('http://192.168.100.65:5000/api/data')
-//       .then(response => response.json())
-//       .then(json => {
-//         setData(json);
-//         console.log(json)
-//       })
-//       .catch(error => {
-//         console.error(error);
-//       });
-//   }, []);
-
-//   return (
-//     <View>
-//       {data && (
-//         <View>
-//           <Text>시가총액: {data['시가총액']}</Text>
-//         </View>
-//       )}
-//     </View>
-//   );
-// };
-
-// export default App;
 import React from 'react';
-import { View, TextInput, Button, StyleSheet,Image,Text } from 'react-native';
+import { View, TextInput, Button, StyleSheet,Image,Text, Alert } from 'react-native';
 
 const App = () => {
   const [username, setUsername] = React.useState('');
   const [password, setPassword] = React.useState('');
 
   const handleLogin = () => {
+    const passwordRegex= /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
     // 로그인 처리 로직을 작성합니다.
-    console.log('Username:', username);
-    console.log('Password:', password);
-    // ... 추가적인 로그인 처리 로직
+    if(username===''){
+      Alert.alert("아이디를 입력해주세요.")
+    }
+    else{
+      if(password===''){
+        Alert.alert("비밀번호를 입력해주세요.")
+      }
+      else if(!passwordRegex.test(password)){
+        Alert.alert('암호는 최소 8자 이상으로 입력해야하며 최소 하나의 대문자와 하나의 숫자를 포함하여 입력해주세요')
+      }
+      else{
+        Alert.alert("유효성 검사 성공")
+      }
+      // ... 추가적인 로그인 처리 로직
+    }
   };
 
   return (
@@ -180,7 +45,7 @@ const App = () => {
         placeholder="Password"
         value={password}
         onChangeText={setPassword}
-        secureTextEntry
+        secureTextEntry // 패스워드타입
       />
       <Button title="Login" onPress={handleLogin} />
       <View style={styles.signUp}>
