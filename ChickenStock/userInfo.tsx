8822bdcb863c478fa9111966830c9290eb031c16
JseopKim <jsub5646@gmail.com>
2023-05-30 16:53:43 +0900

    text 변경

--- ChickenStock/userInfo.tsx
+++ ChickenStock/userInfo.tsx
@@ -58,16 +58,16 @@ export const ChoicePageOne: React.FC = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>어떤 주식 스타일을 선호하시나요?</Text>
-      <TouchableOpacity style={styles.button} onPress={() => handleChoice('Choice 1')}>
+      <TouchableOpacity style={styles.button} onPress={() => handleChoice('안정적')}>
         <Text style={styles.text}>안정적인 투자 스타일</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={() => handleChoice('Choice 2')}>
+      <TouchableOpacity style={styles.button} onPress={() => handleChoice('수익 분배')}>
         <Text style={styles.text}>수익 분배형 투자 스타일</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={() => handleChoice('Choice 3')}>
+      <TouchableOpacity style={styles.button} onPress={() => handleChoice('수익 중심')}>
         <Text style={styles.text}>수익 중심형 투자 스타일</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={() => handleChoice('Choice 4')}>
+      <TouchableOpacity style={styles.button} onPress={() => handleChoice('공격적')}>
         <Text style={styles.text}>공격적인 투자 스타일</Text>
       </TouchableOpacity>
     </View>
@@ -85,19 +85,19 @@ export const ChoicePageTwo: React.FC = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>어떤 주식 종목에 관심 있으신가요? </Text>
-      <TouchableOpacity style={styles.button} onPress={() => handleChoice('Choice 1')}>
+      <TouchableOpacity style={styles.button} onPress={() => handleChoice('제조')}>
         <Text style={styles.text}>제조</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={() => handleChoice('Choice 2')}>
+      <TouchableOpacity style={styles.button} onPress={() => handleChoice('건설')}>
         <Text style={styles.text}>건설</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={() => handleChoice('Choice 3')}>
+      <TouchableOpacity style={styles.button} onPress={() => handleChoice('엔터테인먼트')}>
         <Text style={styles.text}>엔터테인먼트</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={() => handleChoice('Choice 4')}>
+      <TouchableOpacity style={styles.button} onPress={() => handleChoice('금융')}>
         <Text style={styles.text}>금융</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={() => handleChoice('Choice 5')}>
+      <TouchableOpacity style={styles.button} onPress={() => handleChoice('IT')}>
         <Text style={styles.text}>IT / 반도체</Text>
       </TouchableOpacity>
     </View>
@@ -115,13 +115,13 @@ export const ChoicePageThree: React.FC = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>주식 투자를 해보신 적 있으신가요? </Text>
-      <TouchableOpacity style={styles.button} onPress={() => handleChoice('Choice 1')}>
+      <TouchableOpacity style={styles.button} onPress={() => handleChoice('처음')}>
         <Text style={styles.text}>처음입니다</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={() => handleChoice('Choice 2')}>
+      <TouchableOpacity style={styles.button} onPress={() => handleChoice('해보긴 해봤어요')}>
         <Text style={styles.text}>해보긴 해봤어요</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={() => handleChoice('Choice 3')}>
+      <TouchableOpacity style={styles.button} onPress={() => handleChoice('많이 해봤어요')}>
         <Text style={styles.text}>많이 해봤습니다.</Text>
       </TouchableOpacity>
     </View>
@@ -139,13 +139,13 @@ export const ChoicePageFour: React.FC = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>주식의 구조, 위험에 대해 알고 계신가요? </Text>
-      <TouchableOpacity style={styles.button} onPress={() => handleChoice('Choice 1')}>
+      <TouchableOpacity style={styles.button} onPress={() => handleChoice('이해하지 못해요')}>
         <Text style={styles.text}>이해하지 못해요.</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={() => handleChoice('Choice 2')}>
+      <TouchableOpacity style={styles.button} onPress={() => handleChoice('일정 부분 이해합니다')}>
         <Text style={styles.text}>일정 부분 이해합니다.</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={() => handleChoice('Choice 3')}>
+      <TouchableOpacity style={styles.button} onPress={() => handleChoice('깊이 있게 이해해요s')}>
         <Text style={styles.text}>깊이 있게 이해합니다.</Text>
       </TouchableOpacity>
     </View>
