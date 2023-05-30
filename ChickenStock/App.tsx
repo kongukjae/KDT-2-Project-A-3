2c1fa71e5c8a048c8f12e7daaaf62f5ec8a72f6f
KangJeemin <wlals1234560@gmail.com>
2023-05-30 17:58:58 +0900

    네이게이터 어떻게 써야할까?

--- ChickenStock/App.tsx
+++ ChickenStock/App.tsx
@@ -1,15 +1,58 @@
 
-import React from 'react';
-import { View, TextInput, Button, StyleSheet,Image,Text, Alert } from 'react-native';
-import login from './login_module';
+// import React from 'react';
+// import { View, TextInput, Button, StyleSheet,Image,Text, Alert } from 'react-native';
+// import {NavigationContainer} from '@react-navigation/native' 
+// import { createStackNavigator } from '@react-navigation/stack';
+// import login from './login_module';
 
-const App = () => {
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
+// const App = () => {
+
+//   return (
+    
+//     login()
+
+//   )
+// };
+
+
+// export default App;
+
+import React from 'react';
+import { View, Button, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const HomeScreen = ({ navigation }) => {
   return (
-    login()
-  )
+    <View>
+      <Text>홈 화면</Text>
+      <Button
+        title="상세 화면으로 이동"
+        onPress={() => navigation.navigate('Detail')}
+      />
+    </View>
+  );
 };
 
+const DetailScreen = () => {
+  return (
+    <View>
+      <Text>상세 화면</Text>
+    </View>
+  );
+};
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Detail" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default App;
