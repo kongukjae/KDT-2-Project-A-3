bbd0e56387d1cefff4ddca5ab6cb6641e0626ff4
KangJeemin <wlals1234560@gmail.com>
2023-05-31 16:40:36 +0900

    리액트와 파이썬 데이터 주고 받기 완료

---
+++ login.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, request
+# from pymongo import MongoClient
+
+
+app = Flask(__name__)
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = {
+        'message': 'Hello from Flask!',
+        'data': [1, 2, 3, 4, 5]
+    }
+    return jsonify(data)
+
+@app.route('/api/data', methods=['POST'])
+def post_data():
+    request_data = request.get_json()
+    # 여기서 request_data를 사용하여 필요한 작업을 수행합니다.``
+    response_data = {
+        'message': 'Data received successfully!',
+        'data': request_data
+    }
+    print(request_data)
+    return jsonify(response_data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000)
