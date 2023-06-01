42a4f77c8750a62ebb8d21698a8fb4faf929d95c
KangJeemin <wlals1234560@gmail.com>
2023-06-01 12:04:02 +0900

    로그인 로직 수정

--- login.py
+++ login.py
@@ -14,16 +14,33 @@ app = Flask(__name__)
 @app.route('/login', methods=['POST'])
 def post_data():
     request_data = request.get_json() #request.get_json()오로 리액트로부터 데이터 받아옴
-    fromReactUserData=[] 
+    returnValue={} 
     client=MongoClient("mongodb://localhost:27017")
     db=client['test-db']
-    print(request_data["id"])
-    print(db.post.find_one({'author':request_data["id"]}))
+    # fromReactUserData.append(request_data["id"])
+    # fromReactUserData.append(request_data["pw"])
+
+    if db.post.find_one({'author':request_data["id"]})==None:
+        returnValue['state']=False;
+        returnValue['message']="일치하는 아이디가 없음" 
+        return jsonify(returnValue)
+    elif db.post.find_one({'author':request_data["id"]})['author']==db.post.find_one({'author':request_data["id"]}) :
+        if db.post.find_one({'author':request_data["id"]})['text']==
+
+    else: 
+        if not db.post.find_one({'author':request_data["id"]})['text']==request_data['pw']:
+            returnValue['state']=False;
+            returnValue['message']="비밀번호 오류" 
+            return jsonify(returnValue)
+
+        
+
+        
 
     
 
     # print(request_data)
-    return jsonify() #클라언트에게 데이터를 반환.
+    # return jsonify() #클라언트에게 데이터를 반환.
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000,debug=True)
