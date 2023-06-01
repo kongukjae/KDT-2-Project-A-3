067439fb1d38db61bb9d48e8e1dcfb523cb12498
KangJeemin <wlals1234560@gmail.com>
2023-06-01 15:21:08 +0900

    로그인 로직상의 문제 발생

--- login.py
+++ login.py
@@ -16,8 +16,9 @@ def post_data():
     request_data = request.get_json() #request.get_json()오로 리액트로부터 데이터 받아옴
     pattern = r'^[a-zA-Z0-9]+$' #영문자와 숫자로만 입력된 값만 입력.
     returnValue={} 
-    client=MongoClient("mongodb://localhost:27017")
-    db=client['test-db']
+    client=MongoClient("mongodb+srv://ChickenStock:1234@jiseop.g8czkiu.mongodb.net/")
+    db=client['chicken_stock']
+    
 
     if not re.match(pattern,request_data['id']):
         returnValue['state']=False;
@@ -28,21 +29,25 @@ def post_data():
         returnValue['message']="아이디와 비밀번호 모두 입력해주세요." 
         return jsonify(returnValue)
 
-    elif db.post.find_one({'author':request_data["id"]})==None:
+    elif db.user_info.find_one({'id':request_data["id"]})==None:
         returnValue['state']=False;
         returnValue['message']="등록된 회원이 아닙니다." 
         return jsonify(returnValue)
-
-    else: 
-        if not db.post.find_one({'author':request_data["id"]})['text']==request_data['pw']:
-            returnValue['state']=False;
-            returnValue['message']="비밀번호 오류" 
-            return jsonify(returnValue)
-        else :
+    elif db.user_info.find_one({'id':request_data["id"]})['id']==request_data["id"]:
+        if db.user_info.find_one({"password":request_data["pw"]})['password']==request_data["pw"]:
             returnValue['state']=True;
             returnValue['message']="정상" 
             return jsonify(returnValue)
-
+            
+        else:
+            returnValue['state']=False;
+            returnValue['message']="비밀번호 오류" 
+            return jsonify(returnValue)
+    else: 
+        returnValue['state']=False;
+        returnValue['message']="오류" 
+        return jsonify(returnValue)
+        
 
         
 
@@ -50,8 +55,7 @@ def post_data():
 
     
 
-    # print(request_data)
-    # return jsonify() #클라언트에게 데이터를 반환.
+    return jsonify() #클라언트에게 데이터를 반환.
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000,debug=True)
