d670dcff073bb30af610b339b1b9a4819977ac4f
KangJeemin <wlals1234560@gmail.com>
2023-06-01 10:53:31 +0900

    플라스크와 데이터 베이스 연결

--- login.py
+++ login.py
@@ -14,14 +14,13 @@ def get_data():
 
 @app.route('/login', methods=['POST'])
 def post_data():
-    request_data = request.get_json() #리액트로부터 데이터 받아옴
-    # 여기서 request_data를 사용하여 필요한 작업을 수행합니다.``
+    request_data = request.get_json() #request.get_json()오로 리액트로부터 데이터 받아옴
     response_data = {
         'message': 'Data received successfully!',
         'data': request_data
     }
     print(request_data)
-    return jsonify(response_data)
+    return jsonify(response_data) #클라언트에게 데이터를 반환.
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000)
