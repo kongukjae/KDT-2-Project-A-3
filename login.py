b78b911bd679102aa0944b00e06cf9e15d975c04
KangJeemin <wlals1234560@gmail.com>
2023-06-01 11:13:36 +0900

    리액트로부터 유저 정보를 받아오고 , 배열에 담아 놓아야할까?

--- login.py
+++ login.py
@@ -1,26 +1,29 @@
 from flask import Flask, jsonify, request
-# from pymongo import MongoClient
+from pymongo import MongoClient
 
 
 app = Flask(__name__)
 
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    data = {
-        'message': 'Hello from Flask!',
-        'data': [1, 2, 3, 4, 5]
-    }
-    return jsonify(data)
+# @app.route('/api/data', methods=['GET'])
+# def get_data():
+#     data = {
+        
+#     }
+#     return jsonify(data)
 
 @app.route('/login', methods=['POST'])
 def post_data():
     request_data = request.get_json() #request.get_json()오로 리액트로부터 데이터 받아옴
-    response_data = {
-        'message': 'Data received successfully!',
-        'data': request_data
-    }
-    print(request_data)
-    return jsonify(response_data) #클라언트에게 데이터를 반환.
+    fromReactUserData=[] 
+    client=MongoClient("mongodb://localhost:27017")
+    db=client['test-db']
+    print(request_data["id"])
+    print(db.post.find_one({'author':request_data["id"]}))
+
+    
+
+    # print(request_data)
+    return jsonify() #클라언트에게 데이터를 반환.
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0',port=5000,debug=True)
