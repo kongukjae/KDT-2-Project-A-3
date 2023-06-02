dbdc25f835f76edb22faaa28c0b7125fff7fe247
ujou173 <ujou173@gmail.com>
2023-06-02 12:17:33 +0900

    메인페이지 주식 리스트에 필요한 데이터를 모아둔 객체를 생성하기 위한 모듈화 틀 잡기 및 DB와 연결

---
+++ callApiData/stock_list_data.py
@@ -0,0 +1,15 @@
+import mojito
+from pymongo import MongoClient
+
+key = "PSVT5oQXN4N39r3jhoLtrCiVen4fcJ3p7zOh"
+secret = "OeeQY05O9OEfjuOP2KEtVpbP77p8WKaClPqgOEdSAVdH/FazfG51bqSc97t16uYOsvjb5DzrbqB11cfuMfBXPtwDB2BQqg7otSZAHo61OkobqBGPWJHGOHE/lt+X4WPNhyDiDu06EMiC6t+lvcIrG50t4/alJf7qhfL/dkg8sfOJgC66SDA="
+acc_no = "00000000-01"
+
+broker = mojito.KoreaInvestment(api_key=key, api_secret=secret, acc_no=acc_no)
+symbols = broker.fetch_kosdaq_symbols()
+
+client = MongoClient(
+    'mongodb+srv://ChickenStock:1234@jiseop.g8czkiu.mongodb.net/')
+db = client['chicken_stock']
+
+collection = db['business_name_list']
