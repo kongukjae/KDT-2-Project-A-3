90358b8cbe7f87caf105584d9275bb4fd8114b52
JseopKim <jsub5646@gmail.com>
2023-06-09 14:12:17 +0900

    뉴스 슬라이드 진행중

--- ChickenStock/NewsComponent.tsx
+++ ChickenStock/NewsComponent.tsx
@@ -6,19 +6,49 @@ const colors = ['red', 'blue', 'green']; // 페이지 색상 배열
 const SlideComponent = () => {
   const [currentPage, setCurrentPage] = useState(0);
 
+  // function newsData(data) {
+  //   // 데이터 가공 및 활용 예시
+  //   const processedData = data.map(item => {
+  //     return {
+  //       title: item.title,
+  //       detail: item.detail,
+  //       link: item.link
+  //     };
+  //   });
+
+  //   console.log(processedData);
+  // }
+
+  const loadLocation = () => {
+    fetch('http://10.0.2.2:5000/news')
+    .then(response =>  {
+      return response.json()
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+
+  useEffect(() => {
+    loadLocation();
+  }, [])
+
   useEffect(() => {
     const timer = setInterval(() => {
-      setCurrentPage((prevPage) => (prevPage + 1) % colors.length); // 현재 페이지를 다음 페이지로 업데이트
+      setCurrentPage((prevPage) => (prevPage + 1) % colors.length); //? 현재 페이지를 다음페이지로 업데이트 -> 0일 때 1, 1일 때 2, 2일 때 0 순
     }, 3000);
 
-    return () => clearInterval(timer);
+    return () => clearInterval(timer); //? 타이머 초기화
   }, []);
 
   useEffect(() => {
     if (currentPage === colors.length - 1) {
       const resetTimer = setTimeout(() => {
         setCurrentPage(0);
-      }, 3000);
+      }, 3000); //? 만약 현재 페이지가 2일 경우 현재 페이지를 0으로 하고 타이머 초기화
 
       return () => clearTimeout(resetTimer);
     }
@@ -26,8 +56,7 @@ const SlideComponent = () => {
 
   return (
     <View style={styles.container}>
-      <View style={[styles.page, { backgroundColor: colors[currentPage] }]} />
-      <Text style={styles.currentPageText}>Current Page: {currentPage}</Text>
+      <View style={[styles.page, { backgroundColor: colors[currentPage] }]}/>
     </View>
   );
 };
