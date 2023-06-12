b7b33f3928368319eceab77716cd773d892a53e4
KangJeemin <wlals1234560@gmail.com>
2023-06-12 13:01:15 +0900

    모달창 기본 구현

--- ChickenStock/modal.tsx
+++ ChickenStock/modal.tsx
@@ -1,96 +1,126 @@
-import React, { useState } from 'react';
-import { View, Modal, Button, StyleSheet } from 'react-native';
+// import React, { useState } from 'react';
+// import { View, Modal, Button, StyleSheet } from 'react-native';
 
-const MyModal = () => {
+// const MyModal = () => {
+//   const [modalVisible, setModalVisible] = useState(false);
+
+//   return (
+//     <View style={styles.container}>
+//       <Modal
+//         visible={modalVisible}
+//         animationType="slide"
+//         transparent={true}
+//         onRequestClose={() => setModalVisible(false)}
+//       >
+//         <View style={styles.modalContainer}>
+//           <View style={styles.modalContent}>
+//             <View style={styles.rowContainer}>
+//               <View style={styles.column} />
+//               <View style={styles.column} />
+//               <View style={styles.column} />
+//               <View style={styles.column} />
+//               <View style={styles.column} />
+//             </View>
+//             <View style={styles.rowContainer}>
+//               <View style={styles.column} />
+//               <View style={styles.column} />
+//             </View>
+//             <View style={styles.rowContainer}>
+//               <View style={styles.column} />
+//               <View style={styles.column} />
+//             </View>
+//             <View style={styles.rowContainer}>
+//               <View style={styles.column} />
+//               <View style={styles.column} />
+//               <View style={styles.column} />
+//               <View style={styles.column} />
+//               <View style={styles.column} />
+//             </View>
+//             <View style={styles.rowContainer}>
+//               <View style={styles.column} />
+//               <View style={styles.column} />
+//               <View style={styles.column} />
+//               <View style={styles.column} />
+//               <View style={styles.column} />
+//             </View>
+//           </View>
+//         </View>
+//       </Modal>
+      
+//       {!modalVisible && (
+//         <Button
+//           title="Toggle Modal"
+//           onPress={() => setModalVisible(true)}
+//           style={styles.button}
+//         />
+//       )}
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'flex-start',
+//     alignItems: 'center',
+//     marginTop: 20,
+//   },
+//   modalContainer: {
+//     flex: 1,
+//     justifyContent: 'flex-end',
+//     alignItems: 'center',
+//     backgroundColor: 'rgba(0, 0, 0, 0.5)',
+//   },
+//   modalContent: {
+//     backgroundColor: 'white',
+//     width: '100%',
+//     height: '50%',
+//     paddingHorizontal: 10,
+//     paddingTop: 10,
+//   },
+//   rowContainer: {
+//     flexDirection: 'row',
+//   },
+//   column: {
+//     flex: 1,
+//     height: 50,
+//     borderWidth: 1,
+//     borderColor: 'black',
+//   },
+//   button: {
+//     position: 'absolute',
+//     top: 0,
+//   },
+// });
+
+// export default MyModal;
+
+import React, { useState } from 'react';
+import { Button, Modal, Text, View } from 'react-native';
+
+const App = () => {
   const [modalVisible, setModalVisible] = useState(false);
 
+  const openModal = () => {
+    setModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
+  };
+
   return (
-    <View style={styles.container}>
-      <Modal
-        visible={modalVisible}
-        animationType="slide"
-        transparent={true}
-        onRequestClose={() => setModalVisible(false)}
-      >
-        <View style={styles.modalContainer}>
-          <View style={styles.modalContent}>
-            <View style={styles.rowContainer}>
-              <View style={styles.column} />
-              <View style={styles.column} />
-              <View style={styles.column} />
-              <View style={styles.column} />
-              <View style={styles.column} />
-            </View>
-            <View style={styles.rowContainer}>
-              <View style={styles.column} />
-              <View style={styles.column} />
-            </View>
-            <View style={styles.rowContainer}>
-              <View style={styles.column} />
-              <View style={styles.column} />
-            </View>
-            <View style={styles.rowContainer}>
-              <View style={styles.column} />
-              <View style={styles.column} />
-              <View style={styles.column} />
-              <View style={styles.column} />
-              <View style={styles.column} />
-            </View>
-            <View style={styles.rowContainer}>
-              <View style={styles.column} />
-              <View style={styles.column} />
-              <View style={styles.column} />
-              <View style={styles.column} />
-              <View style={styles.column} />
-            </View>
-          </View>
+    <View>
+      <Button title="Open Modal" onPress={openModal} />
+
+      <Modal visible={modalVisible} animationType="slide">
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>Hello, Modal!</Text>
+          <Button title="Close" onPress={closeModal} />
         </View>
       </Modal>
-      
-      {!modalVisible && (
-        <Button
-          title="Toggle Modal"
-          onPress={() => setModalVisible(true)}
-          style={styles.button}
-        />
-      )}
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    marginTop: 20,
-  },
-  modalContainer: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
-  modalContent: {
-    backgroundColor: 'white',
-    width: '100%',
-    height: '50%',
-    paddingHorizontal: 10,
-    paddingTop: 10,
-  },
-  rowContainer: {
-    flexDirection: 'row',
-  },
-  column: {
-    flex: 1,
-    height: 50,
-    borderWidth: 1,
-    borderColor: 'black',
-  },
-  button: {
-    position: 'absolute',
-    top: 0,
-  },
-});
-
-export default MyModal;
+export default App;
